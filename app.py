d094977dcf693b9ffc82a69ab46238b74bd753ab
AdityaK1301 <112890345+AdityaK1301@users.noreply.github.com>
2025-12-15 18:19:37 +0530

    Add files via upload
    updated app and index

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, Response, jsonify, request, url_for, render_template, flash, redirect
 from db import get_db_connection
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-@app.route('/')
-def Index():
-    return render_template('index.html')
+def get_current_date_time():
+    now = datetime.now()
+    return now.strftime('%Y-%m-%d'), now.strftime('%I:%M:%S %p')
 
-@app.route('/create', methods=['POST'])
-def create():
+@app.route('/', methods=['GET', 'POST'])
+def Index():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * from users")
+    fetch = cursor.fetchall()
+
     msg = None
     msg_type = None 
     if request.method == 'POST':
@@ -21,12 +27,16 @@ def create():
             flash('All field required', 'danger')
             return redirect(url_for('create'))
         
-        conn = get_db_connection()
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute("INSERT INTO `users` (`name`, `email`, `mobile`, `password`) VALUES ('', '', '', '');")
+        add_date, add_time = get_current_date_time()
+        
+        
+        cursor.execute("INSERT INTO `users` (`name`, `email`, `mobile`, `password`, `add_date`, `add_time`) VALUES (%s, %s, %s, %s, %s, %s)", (name, email, mobile, password, add_date, add_time))
+        conn.commit()
+        cursor.close()
+        conn.close()
 
+    return render_template('index.html', fetch=fetch)
 
- 
 
 if __name__ == "__main__":
     app.run(debug=True, port=6002)
